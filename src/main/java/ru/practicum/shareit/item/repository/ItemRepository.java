76ffe74e340043d1be183197d2637dd760d41c96
Nevsky <nevisky@mail.ru>
2023-07-16 02:11:47 +0300

    ТЗ-15 update app

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -3,11 +3,9 @@ package ru.practicum.shareit.item.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @Repository
