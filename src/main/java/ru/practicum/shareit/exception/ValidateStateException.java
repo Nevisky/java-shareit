82846631d0e95f269f6a63c046afc075a392e049
Nevsky <nevisky@mail.ru>
2023-07-24 00:54:01 +0300

    ТЗ 16 add-docker delere ex-src

--- src/main/java/ru/practicum/shareit/exception/ValidateStateException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class ValidateStateException extends RuntimeException {
-    public ValidateStateException(String message) {
-        super(message);
-    }
-}
